5f0e22fb535683b05019874fbed213f393e638a5
Mads Iversen <madsiversen@hotmail.com>
2019-08-11 23:42:49 +0200

    har tilføje 4 funktioner som tilføjer classen hide til de container i portfolio sectionen som ikke er blevet fremkald af et click event

--- script.js
+++ script.js
@@ -21,3 +21,35 @@ function sideVises() {
     document.querySelector("#tema3-btn").addEventListener("click", hideTemaContainers124);
     document.querySelector("#tema4-btn").addEventListener("click", hideTemaContainers123);
 }
+
+function hideTemaContainers123() {
+    console.log("hideTemaContainers123");
+    //hide tema container 1, 2 og 3 når der klikkes på tema4 knap
+    document.querySelector("#tema-container1").classList.add("hide");
+    document.querySelector("#tema-container2").classList.add("hide");
+    document.querySelector("#tema-container3").classList.add("hide");
+}
+
+function hideTemaContainers234() {
+    console.log("hideTemaContainers234");
+    //hide tema container 2, 3 og 4 når der klikkes på tema1 knap
+    document.querySelector("#tema-container2").classList.add("hide");
+    document.querySelector("#tema-container3").classList.add("hide");
+    document.querySelector("#tema-container4").classList.add("hide");
+}
+
+function hideTemaContainers134() {
+    console.log("hideTemaContainers134");
+    //hide tema container 1, 3 og 4 når der klikkes på tema2 knap
+    document.querySelector("#tema-container1").classList.add("hide");
+    document.querySelector("#tema-container3").classList.add("hide");
+    document.querySelector("#tema-container4").classList.add("hide");
+}
+
+function hideTemaContainers124() {
+    console.log("hideTemaContainers124");
+    //hide tema container 1, 2 og 4 når der klikkes på tema3 knap
+    document.querySelector("#tema-container1").classList.add("hide");
+    document.querySelector("#tema-container2").classList.add("hide");
+    document.querySelector("#tema-container4").classList.add("hide");
+}
